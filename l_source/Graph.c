486112d38e23a5ad40aee401be576124baa4c831
Sam Bagot <sambagot99@gmail.com>
2019-04-05 11:36:34 +1100

    functions added to graph

--- l_source/Graph.c
+++ l_source/Graph.c
@@ -2,19 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-Graph newGraph(int noNodes);
+Graph newGraph(int noNodes) {
+    Graph g = malloc(sizeof(Graph));
+    while (
+    return g;
+}
 
 void  insertEdge(Graph g, Vertex src, Vertex dest, int weight) {
     
 }
-void  removeEdge(Graph g, Vertex src, Vertex dest);
-bool  adjacent(Graph g, Vertex src, Vertex dest);
-int  numVerticies(Graph g);
+void  removeEdge(Graph g, Vertex src, Vertex dest) {
+    
+}
+bool  adjacent(Graph g, Vertex src, Vertex dest) {
+    
+    return ;
+}
+int  numVerticies(Graph g) { 
 
-AdjList outIncident(Graph g, Vertex v);
+}
 
-AdjList inIncident(Graph g, Vertex v);
+AdjList outIncident(Graph g, Vertex v) { 
 
-void  showGraph(Graph g);
+}
 
-void  freeGraph(Graph g);
+AdjList inIncident(Graph g, Vertex v) {
+
+}
+
+void  showGraph(Graph g) {
+
+}
+
+void  freeGraph(Graph g) {
+
+}
