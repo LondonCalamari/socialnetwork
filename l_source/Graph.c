064261e18919d78fc630fb020315219748bbd0f3
Sam Bagot <sambagot99@gmail.com>
2019-04-05 11:07:59 +1100

    added function names to Graph.c

--- l_source/Graph.c
+++ l_source/Graph.c
@@ -1,3 +1,16 @@
 #include <Graph.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+void  insertEdge(Graph g, Vertex src, Vertex dest, int weight);
+void  removeEdge(Graph g, Vertex src, Vertex dest);
+bool  adjacent(Graph g, Vertex src, Vertex dest);
+int  numVerticies(Graph g);
+
+AdjList outIncident(Graph g, Vertex v);
+
+AdjList inIncident(Graph g, Vertex v);
+
+void  showGraph(Graph g);
+
+void  freeGraph(Graph g);
