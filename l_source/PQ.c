aaf8f2c78522e247907a6be5ae6a86aea2eb62ea
CalamariAndChips <z5122528@student.unsw.edu.au>
2019-04-08 11:14:40 +1000

    read comment in additem

--- l_source/PQ.c
+++ l_source/PQ.c
@@ -26,11 +26,24 @@ PQ newPQ() {
 
 // Adds item (ItemPQ) to the priority queue. If an item with 'key' exists, it's 'value' is updated.
 void  addPQ(PQ pq, ItemPQ item) {
+    // go through to see if item with key exists, update if it does
+    // im so confused because this is the only way to do this in a heap but it
+    // essentially makes our heap pointless as it makes the complexity worse
+    for (int i = 1; i <= pq->nitems; i++) {
+        if (pq->nitems[i].key == item.key) {
+            pq->nitems[i].value == item.value;
+        }
+    }
+
     if (pq->nitems + 1 >= pq->size) {
         pq->size = pq->size * 2;
         pq->nodes = realloc(pq->nodes, pq->size * sizeof(ItemPQ));
     }
-    int child = pq->nitems + 1; // start from [1]
+    // Insert the item into last place
+    pq->nodes[nitems] = item;
+
+    // Put it into the correct position
+    int child = pq->nitems; // start from [1]
     int parent = child / 2;
     while (child > 1 && pq->nodes[parent].value > item.value) {
         pq->nodes[child] = pq->nodes[parent];
@@ -38,10 +51,28 @@ void  addPQ(PQ pq, ItemPQ item) {
         parent = parent / 2;
     }
     pq->nodes[child] = item;
-   }
+}
 
 //Removes and returns the item (ItemPQ) with smallest 'value'. Returns null if this queue is empty.
 ItemPQ  dequeuePQ(PQ pq) {
+    int minItem, lastItem, child, curr;
+    minItem = pq->nodes[1];
+    lastItem = pq->nodes[pq->nitems];
+
+    // replace nodes[1] with new minimum by comparing the value to its children
+    for (curr = 1; curr*2 <= pq->nitems; curr = child) {
+        child = curr * 2;
+        if (child != pq->nitems && pq->nodes[child+1].value < pq->nodes[child]) {
+            child++;
+        }
+        if (lastItem > pq->nodes[child]) {
+            pq->nodes[curr] = pq->nodes[child];
+        } else { break; }
+    }
+    pq->nodes[child] = lastItem;
+    return minItem;
+    
+    /*
     if (pq == NULL) { return NULL; }
     if (pq->nodes == NULL || pq->len == 0) { return NULL; }
     int smallest_node = pq->nodes[0].value;
@@ -52,6 +83,7 @@ ItemPQ  dequeuePQ(PQ pq) {
     } 
     /// NEEDS TO NOW REMOVE THE ITEM
     return pq->nodes[smallest_node];
+    */
 }
 
 // Updates item with a given 'key' value, by updating that item's value to the given 'value'.
