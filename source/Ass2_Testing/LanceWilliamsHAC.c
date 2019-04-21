f71dfd0897f7e7c4db584ef60a416838141e800a
LondonCalamari <z5122528@student.unsw.edu.au>
2019-04-21 13:28:03 +1000

    started Lance

--- source/Ass2_Testing/LanceWilliamsHAC.c
+++ source/Ass2_Testing/LanceWilliamsHAC.c
@@ -7,6 +7,7 @@
 #include "LanceWilliamsHAC.h"
 #include "Graph.h"
 #define numVertices numVerticies
+#define MAX 9999
 
 /* 
  * Finds Dendrogram using Lance-Williams algorithm (as discussed in the specs) 
@@ -18,13 +19,53 @@
  * 
  */
 Dendrogram LanceWilliamsHAC(Graph g, int method) {
+    double distances[numVerticies(g)][numVerticies(g)] = {0};
+    for (int i = 0; i < numVerticies(g); i++) {
+        for (int j = 0; j < numVerticies(g); j++) {
+            distances[i][j] = MAX;
+        }
+    }
+    // Calculate distances between each pair of vertices
+    for (int i = 0; i < numVerticies(g); i++) {
+        AdjList adj = outIncident(g, i);
+        int col = 0;
+        while (adj->next != NULL) {
+            distances[i][col] = 1/(double)adj->weight;
+            col++;
+        }
+    }    
+    // get the max dist if there are two links
+    for (int i = 0; i < numVerticies(g); i++) {
+        for (int j = 0; j < numVerticies(g); i++) {
+            if (distances[i][j] <= distances[j][i]) {
+                distances[i][j] = distances[j][i];
+            } else {
+                distances[j][i] = distances[i][j];
+            }
+        }
+    }
+    // create clusters for every vertex i ????
+    for (int i = 0; i < numVerticies(g); i++) {
+        DNode *node = malloc(sizeof(DNode));
+        node->vertex = i;
+        node->left = node->right = NULL;
+    }
+
+    
+
 
     return NULL;
 }
 
+// recursively free the dendrogram
+static Dendrogram recursiveFree(Dendrogram d) {
+    if (d == NULL) { return NULL; }
+    recursiveFree(d->left);
+    recursiveFree(d->right);
+}
 
 void freeDendrogram(Dendrogram d) {
-
+    recursiveFree(d);
 	return;
 }
 
