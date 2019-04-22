0a6bbbe7e57ef0087cffb0180d4fb1db6f94e1d9
LondonCalamari <z5122528@student.unsw.edu.au>
2019-04-22 18:07:28 +1000

    More dendrogram progress

--- source/Ass2_Testing/LanceWilliamsHAC.c
+++ source/Ass2_Testing/LanceWilliamsHAC.c
@@ -18,8 +18,12 @@
    The function returns 'Dendrogram' structure (binary tree) with the required information.
  * 
  */
+// static void smallestDist(double *row, double *col, double *dist);
+static Dendrogram newDNODE(int v);
+
 Dendrogram LanceWilliamsHAC(Graph g, int method) {
-    double distances[10][10/*numVerticies(g)*/] = {0};
+    // create 2D array to hold all distances
+    double distances[numVerticies(g)][numVerticies(g)]; // = {1};
     for (int i = 0; i < numVerticies(g); i++) {
         for (int j = 0; j < numVerticies(g); j++) {
             distances[i][j] = MAX;
@@ -44,18 +48,84 @@ Dendrogram LanceWilliamsHAC(Graph g, int method) {
             }
         }
     }
-    // create clusters for every vertex i ????
+    
+    // Array to store dendro pointers 
+    int dendASize = numVerticies(g);
+    Dendrogram *dendA = malloc(sizeof(Dendrogram) * dendASize);
+
+    // Array to know which cols to skip in dendA
+    int skip[MAX] = {0};
+    // create clusters for every vertex i
     for (int i = 0; i < numVerticies(g); i++) {
-        DNode *node = malloc(sizeof(DNode));
-        node->vertex = i;
-        node->left = node->right = NULL;
+        Dendrogram node = newDNODE(i);
+        dendA[i] = node;
     }
 
+    // For K=1 to N-1 ?? 
+    for (int i = 0; i < numVerticies(g); i++) {
+        // find two closest clusters using dist array
+        int row, col; 
+        double min = MAX;
+        for (int x; x < numVerticies(g); x++) {
+            for (int y; y < numVerticies(g); y++) {
+                // only need to look half of matrix
+                if (x > y) {
+                    if (distances[x][y] < min) { 
+                        min = distances[x][y]; 
+                        row = x;
+                        col = y;
+                    }
+                }
+            }
+        }
+
+        // Create new col in dendA for cluster
+        dendASize++;
+        skip[row] = 1; skip[col] = 1;
+        dendA = realloc(dendA, dendASize * sizeof(Dendrogram));
+        Dendrogram new = newDNODE(MAX);
+        new->left = dendA[row]; new->right = dendA[col];
+        dendA[dendASize-1] = new;
+
+        // Update distance using LW Single-link Method
+        double new_dist = distances[0][0];
+
+        // Update dendrogram
+
+    }
     
 
 
-    return NULL;
+    return dendA[0];
 }
+
+Dendrogram newDNODE(int v) {
+    Dendrogram node = malloc(sizeof(Dendrogram));
+    node->vertex = v;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+/*
+// Find the smallest value in distance[][] 
+static void smallestDist(double *row, double *col, double *dist) {
+    double min = 9999;
+    for (int x; x < numVerticies(g); i++) {
+        for (int y; y < numVerticies(g); y++) {
+            // only need to look half of matrix
+            if (x > y) {
+                if (dist[x][y] < min) { 
+                    min = dist[x][y]; 
+                    row = x;
+                    col = y;
+                }
+            }
+        }
+    }
+    
+}
+*/
 
 // recursively free the dendrogram
 void freeDendrogram(Dendrogram d) {
